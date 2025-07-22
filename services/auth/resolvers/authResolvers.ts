4c76420ad9415250d0f6a7e4c38caac4881fdfec
Jason Ross <jason.ross841@gmail.com>
2025-07-22 17:17:26 -0500

    adding login GraphQL resolver

--- services/auth/resolvers/authResolvers.ts
+++ services/auth/resolvers/authResolvers.ts
@@ -1,36 +1,26 @@
-function loginUserResolver() {
-    return {
-        Subscription: {
-            loginUser: {
-                subscribe: async ({ email, password }: { email: string; password: string; }, { pubsub }: { pubsub }: { EventEmitter }) => {
-                    loginUser({ email, password });
-                    return pubsub.asyncIterator('LOGIN_SUCCESS' || 'LOGIN_FAILURE');
-                }
-            }
-        }
-    };
+
+type userLoginInput = {
+  email: String!
+  password: String!
 }
 
-function logoutUserResolver() {
-    return {
-        Subscription: {
-            logoutUser: {
-                subscribe: async ({ email }: { email: string }, { pubsub }: { pubsub }: { EventEmitter }) => {
-                    logoutUser({ email });
-                    return pubsub.asyncIterator('LOGOUT_SUCCESS' || 'LOGOUT_FAILURE');
-                }
-            }
-        }
-    };
-}
 
-function createUserResolver() {
-    return {
-        Mutation: {
-            createUser: async ({ email, password }: { email: string; password: string; }) => {
-                const user = await createUser({ email, password });
-                return user ?  "USER_CREATED_SUCCESS" : "USER_CREATION_FAILED";
+const resolvers = {
+    Mutation: {
+        login: async (_parent, { input: input }, context) => {
+            const { email, password } = input;
+            // Db connection and authentication logic here
+            const user = await context.db.users.findOne({ email });
+            if (!user || user.password !== password || !user.email) {
+                return { userLoginSuccessStatus: false };
+                else if (user && user.password === password && user.email) {
+                    return { userLoginSuccessStatus: true };
+            }
+            else {
+                return { userLoginSuccessStatus: false };
+                console.log("User not found or unknown error");
+            }
+        },
+                
             }
         }
-    };
-}
